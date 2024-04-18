51c3efd73b49620d1167d77b46c00f56b40a6550
mdatz <kadatzmatthew@gmail.com>
2024-04-18 15:08:31 -0600

    Initial boilerplate

---
+++ main.ts
@@ -0,0 +1,23 @@
+import express from 'npm:express@4.19.2';
+import { apiReference } from 'npm:@scalar/express-api-reference@0.4.0'
+
+const app = express()
+app.use(express.json())
+app.use(express.static('public'));
+
+app.use(
+  '/docs',
+  apiReference({
+    spec: {
+      url: '/openapi.yaml',
+    },
+  }),
+)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(2077, () => {
+  console.log('Server is running on http://localhost:2077')
+});
